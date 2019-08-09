ef20c24bfcb556d650d36ed9f73b6660bd3fcaed
Leonardo Sbarai <leonardo.sbarai@gmail.com>
2019-08-09 12:18:24 -0300

    Atualização

--- target/cucumber-htmlreport/report.js
+++ target/cucumber-htmlreport/report.js
@@ -1,20 +1,132 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ShareFF.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("DemoFF.feature");
 formatter.feature({
   "line": 1,
-  "name": "Create account of Facebook9",
-  "description": "As a user I need to open facebook home page and do the validations",
-  "id": "create-account-of-facebook9",
+  "name": "Validate first and last name4",
+  "description": "As a user I need to validate first and last name",
+  "id": "validate-first-and-last-name4",
   "keyword": "Feature"
-});
-formatter.before({
-  "duration": 3528689600,
-  "status": "passed"
 });
 formatter.scenario({
   "line": 4,
-  "name": "Validade First Name field91",
+  "name": "Check first name and last name41",
   "description": "",
-  "id": "create-account-of-facebook9;validade-first-name-field91",
+  "id": "validate-first-and-last-name4;check-first-name-and-last-name41",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "User need to be on demo site page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User enters first name",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enters last name",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "Close browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DemoStepDef.user_need_to_be_on_demo_site_page()"
+});
+formatter.result({
+  "duration": 14895408691,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DemoStepDef.user_enters_first_name()"
+});
+formatter.result({
+  "duration": 1187785968,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DemoStepDef.user_enters_last_name()"
+});
+formatter.result({
+  "duration": 1181749812,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultiStepDef.close_browser()"
+});
+formatter.result({
+  "duration": 961761541,
+  "status": "passed"
+});
+formatter.uri("GoogleFF.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Validate search of Google5",
+  "description": "As a user I need to validate a search of Google",
+  "id": "validate-search-of-google5",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Validade search field51",
+  "description": "",
+  "id": "validate-search-of-google5;validade-search-field51",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "User need to be on google page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User enters search string",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "Close browser",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "GoogleStepDef.user_need_to_be_on_google_page()"
+});
+formatter.result({
+  "duration": 4322789760,
+  "status": "passed"
+});
+formatter.match({
+  "location": "GoogleStepDef.user_enters_search_string()"
+});
+formatter.result({
+  "duration": 1171179798,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MultiStepDef.close_browser()"
+});
+formatter.result({
+  "duration": 997018938,
+  "status": "passed"
+});
+formatter.uri("MultiFF.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Create account of Facebook6",
+  "description": "As a user I need to open facebook home page and do the validations",
+  "id": "create-account-of-facebook6",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Validade First Name field61",
+  "description": "",
+  "id": "create-account-of-facebook6;validade-first-name-field61",
   "type": "scenario",
   "keyword": "Scenario"
 });
@@ -34,10 +146,10 @@ formatter.step({
   "keyword": "Then "
 });
 formatter.match({
-  "location": "ShareDataStepDef.user_need_to_be_on_Facebook_login_page()"
+  "location": "MultiStepDef.user_need_to_be_on_Facebook_login_page()"
 });
 formatter.result({
-  "duration": 7390999200,
+  "duration": 5213353424,
   "status": "passed"
 });
 formatter.match({
@@ -47,10 +159,10 @@ formatter.match({
       "offset": 18
     }
   ],
-  "location": "ShareDataStepDef.user_enters_user_first_name(String)"
+  "location": "MultiStepDef.user_enters_user_first_name(String)"
 });
 formatter.result({
-  "duration": 152760300,
+  "duration": 190487703,
   "status": "passed"
 });
 formatter.match({
@@ -60,25 +172,17 @@ formatter.match({
       "offset": 18
     }
   ],
-  "location": "ShareDataStepDef1.user_checks_user_first_name_is_present(String)"
+  "location": "MultiStepDef.user_checks_user_first_name_is_present(String)"
 });
 formatter.result({
-  "duration": 36134500,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 914595000,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 3407592100,
+  "duration": 53374773,
   "status": "passed"
 });
 formatter.scenario({
   "line": 9,
-  "name": "Validate create a user multiple fields92",
+  "name": "Validate create a user multiple fields62",
   "description": "",
-  "id": "create-account-of-facebook9;validate-create-a-user-multiple-fields92",
+  "id": "create-account-of-facebook6;validate-create-a-user-multiple-fields62",
   "type": "scenario",
   "keyword": "Scenario"
 });
@@ -107,11 +211,16 @@ formatter.step({
   "name": "User mobile field should be blank",
   "keyword": "Then "
 });
+formatter.step({
+  "line": 15,
+  "name": "Close browser",
+  "keyword": "Then "
+});
 formatter.match({
-  "location": "ShareDataStepDef.user_need_to_be_on_Facebook_login_page()"
+  "location": "MultiStepDef.user_need_to_be_on_Facebook_login_page()"
 });
 formatter.result({
-  "duration": 6997297900,
+  "duration": 1971744592,
   "status": "passed"
 });
 formatter.match({
@@ -121,10 +230,10 @@ formatter.match({
       "offset": 18
     }
   ],
-  "location": "ShareDataStepDef.user_enters_user_first_name(String)"
+  "location": "MultiStepDef.user_enters_user_first_name(String)"
 });
 formatter.result({
-  "duration": 150041200,
+  "duration": 174951063,
   "status": "passed"
 });
 formatter.match({
@@ -134,10 +243,10 @@ formatter.match({
       "offset": 18
     }
   ],
-  "location": "ShareDataStepDef1.user_enters_user_surname(String)"
+  "location": "MultiStepDef.user_enters_user_surname(String)"
 });
 formatter.result({
-  "duration": 122590700,
+  "duration": 249273094,
   "status": "passed"
 });
 formatter.match({
@@ -147,21 +256,24 @@ formatter.match({
       "offset": 18
     }
   ],
-  "location": "ShareDataStepDef1.user_checks_user_first_name_is_present(String)"
+  "location": "MultiStepDef.user_checks_user_first_name_is_present(String)"
 });
 formatter.result({
-  "duration": 27027500,
+  "duration": 50496449,
   "status": "passed"
 });
 formatter.match({
-  "location": "ShareDataStepDef1.user_mobile_field_should_be_blank()"
+  "location": "MultiStepDef.user_mobile_field_should_be_blank()"
 });
 formatter.result({
-  "duration": 37519600,
+  "duration": 40753702,
   "status": "passed"
 });
-formatter.after({
-  "duration": 861667700,
+formatter.match({
+  "location": "MultiStepDef.close_browser()"
+});
+formatter.result({
+  "duration": 109033634,
   "status": "passed"
 });
 });
